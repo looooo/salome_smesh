60665280b0e70503a0f8611c6e791181bb72736e
enk <enk@opencascade.com>
2006-01-27 09:27:34 +0000

    Fix for RedHat8.0 Bug IPAL10936:   Polygonal face cannot be selected

--- src/SMESHGUI/SMESHGUI_RemoveElementsDlg.cxx
+++ src/SMESHGUI/SMESHGUI_RemoveElementsDlg.cxx
@@ -45,6 +45,7 @@
 #include "SVTK_ViewWindow.h"
 #include "SALOME_ListIO.hxx"
 
+#include "SalomeApp_Tools.h"
 #include "utilities.h"
 
 // OCCT Includes
@@ -257,7 +258,11 @@ void SMESHGUI_RemoveElementsDlg::ClickOnApply()
     try {
       SMESH::SMESH_MeshEditor_var aMeshEditor = myMesh->GetMeshEditor();
       aResult = aMeshEditor->RemoveElements(anArrayOfIdeces.inout());
-    } catch (...) {
+    } catch (const SALOME::SALOME_Exception& S_ex) {
+      SalomeApp_Tools::QtCatchCorbaException(S_ex);
+      myEditCurrentArgument->clear();
+    } catch (...){
+      myEditCurrentArgument->clear();
     }
 
     if (aResult) {

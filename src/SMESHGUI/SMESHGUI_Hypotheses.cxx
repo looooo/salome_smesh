a4c86cd9f4e89a6b7bd3113da8330bd47ed39f24
eap <eap@opencascade.com>
2010-12-22 10:11:01 +0000

    0020918: EDF 1447 SMESH: Mesh common borders    specify help page

--- src/SMESHGUI/SMESHGUI_Hypotheses.cxx
+++ src/SMESHGUI/SMESHGUI_Hypotheses.cxx
@@ -557,6 +557,8 @@ QString SMESHGUI_GenericHypothesisCreator::helpPage() const
     aHelpFileName = "segments_around_vertex_algo_page.html";
   else if ( aHypType == "QuadrangleParams")
     aHelpFileName = "a2d_meshing_hypo_page.html#hypo_quad_params_anchor";
+  else if ( aHypType == "ImportSource1D" || aHypType == "ImportSource2D")
+    aHelpFileName = "import_algos_page.html";
   return aHelpFileName;
 }
 

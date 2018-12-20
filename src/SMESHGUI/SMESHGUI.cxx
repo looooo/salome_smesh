d534a76fef54c099410d8fe4f926662ce2f0c8dc
mkr <margarita.karpunina@opencascade.com>
2018-12-20 11:42:19 +0300

    0054465: TC9.2.0: SMESH: mesh presentation is not fully restored after Controls - Reset command

--- src/SMESHGUI/SMESHGUI.cxx
+++ src/SMESHGUI/SMESHGUI.cxx
@@ -2536,6 +2536,7 @@ bool SMESHGUI::OnGUIEvent( int theCommandID )
 #ifndef DISABLE_PLOT2DVIEWER
             SMESH::ProcessIn2DViewers(anActor,SMESH::RemoveFrom2dViewer);
 #endif
+            anActor->UpdateFilter();
           }
         }
       }
